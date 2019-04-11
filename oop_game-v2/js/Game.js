24708e7a4d4d49cb6f917b0f59c31897405a565e
Douglas Hill <dohill06@gmail.com>
2019-04-11 09:21:43 -0700

    Created the Game class

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -1,3 +1,36 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [];
+        this.activePhrase = null;
+    }
+
+    startGame() {
+
+    }
+
+    getRandomPhrase() {
+
+    }
+
+    handleInteraction() {
+
+    }
+
+    removeLife() {
+
+    }
+
+    checkForWin() {
+
+    }
+
+    gameOver() {
+        
+    }
+
+}
