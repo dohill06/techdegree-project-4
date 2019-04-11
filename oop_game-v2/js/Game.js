51ee83981115a19356d0b03f68cc8a9ea58f58e5
Douglas Hill <dohill06@gmail.com>
2019-04-11 10:19:55 -0700

    Added the createPhrases method

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -5,12 +5,24 @@
 class Game {
     constructor() {
         this.missed = 0;
-        this.phrases = [];
+        this.phrases = createPhrases();
         this.activePhrase = null;
     }
 
     startGame() {
 
+    }
+
+    createPhrases() {
+        const phraseArray = [
+            new Phrase('I drink your milkshake'),
+            new Phrase('A diamond in the rough'),
+            new Phrase('Accidents will happen'),
+            new Phrase('Down in the dumps'),
+            new Phrase('Fire walk with me')
+        ];
+
+        return phraseArray;
     }
 
     getRandomPhrase() {
