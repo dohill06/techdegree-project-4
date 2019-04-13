37fabcb0b167dde8528de507b5c80e43a27dce57
Douglas Hill <dohill06@gmail.com>
2019-04-13 11:40:22 -0700

    Added enter keyup to start button

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # techdegree-project-4
+
+Added enter keyup to start button
--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -5,7 +5,17 @@
 let game;
 const startButton = document.querySelector('#btn__reset');
 
+// Start on click
 startButton.addEventListener('click', () => {
     game = new Game();
     game.startGame();
+});
+
+// Start on enter
+document.addEventListener('keyup', (e) => {
+
+    if (e.keyCode === 13 && startButton.parentNode.style.display === '') {
+        game = new Game();
+        game.startGame();
+    }
 });
